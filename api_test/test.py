01408a0c4509fdd9e81b168411695cbdc232510b
tzai <tzai@123Mac-mini.local>
2026-01-07 14:41:43 +0900

    add score in the result

--- api_test/test.py
+++ api_test/test.py
@@ -3,31 +3,31 @@ import requests
 url = "http://159.135.196.86:12280/streamers"
 with open("./4-3.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_001"}
+    data = {"streamer_id": "streamer_002"}
     resp = requests.post(url, files=files, data=data)
 with open("./4-4.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_001"}
+    data = {"streamer_id": "streamer_002"}
     resp = requests.post(url, files=files, data=data)
 with open("./4-5.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_001"}
+    data = {"streamer_id": "streamer_002"}
     resp = requests.post(url, files=files, data=data)
 with open("./4-6.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_001"}
+    data = {"streamer_id": "streamer_002"}
     resp = requests.post(url, files=files, data=data)
 with open("./4-7.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_001"}
+    data = {"streamer_id": "streamer_002"}
     resp = requests.post(url, files=files, data=data)
 with open("./4-8.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_001"}
+    data = {"streamer_id": "streamer_002"}
     resp = requests.post(url, files=files, data=data)
 with open("./4-9.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_001"}
+    data = {"streamer_id": "streamer_002"}
     resp = requests.post(url, files=files, data=data)
 print(resp.status_code, resp.json())
 
