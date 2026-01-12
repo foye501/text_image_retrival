604d8bbfed03c2d773a47798fe3464ba21fd4e09
tzai <tzai@123Mac-mini.local>
2026-01-12 16:20:45 +0900

    update to store only image uri

--- api_test/test.py
+++ api_test/test.py
@@ -1,35 +1,48 @@
 import requests
 
 url = "http://159.135.196.86:12280/streamers"
-with open("./4-3.png", "rb") as f:
+with open("./20260109-165210.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_002"}
+    data = {"streamer_id": "streamer_005"}
     resp = requests.post(url, files=files, data=data)
-with open("./4-4.png", "rb") as f:
+with open("./20260109-165215.jpeg", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_002"}
+    data = {"streamer_id": "streamer_005"}
     resp = requests.post(url, files=files, data=data)
-with open("./4-5.png", "rb") as f:
+with open("./20260109-165219.jpeg", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_002"}
+    data = {"streamer_id": "streamer_005"}
     resp = requests.post(url, files=files, data=data)
-with open("./4-6.png", "rb") as f:
+with open("20260109-165224.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_002"}
+    data = {"streamer_id": "streamer_005"}
     resp = requests.post(url, files=files, data=data)
-with open("./4-7.png", "rb") as f:
+with open("./20260109-165228.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_002"}
+    data = {"streamer_id": "streamer_005"}
     resp = requests.post(url, files=files, data=data)
-with open("./4-8.png", "rb") as f:
-    files = {"image": f}
-    data = {"streamer_id": "streamer_002"}
+# with open("20260109-165232.png", "rb") as f:
+#     files = {"image": f}
+#     data = {"streamer_id": "streamer_005"}
     resp = requests.post(url, files=files, data=data)
-with open("./4-9.png", "rb") as f:
+with open("./20260109-165252.png", "rb") as f:
     files = {"image": f}
-    data = {"streamer_id": "streamer_002"}
+    data = {"streamer_id": "streamer_005"}
+    resp = requests.post(url, files=files, data=data)
+with open("20260109-165313.png", "rb") as f:
+    files = {"image": f}
+    data = {"streamer_id": "streamer_005"}
+    resp = requests.post(url, files=files, data=data)
+with open("./20260109-165344.jpeg", "rb") as f:
+    files = {"image": f}
+    data = {"streamer_id": "streamer_005"}
+    resp = requests.post(url, files=files, data=data)
+with open("./20260109-165831.jpeg", "rb") as f:
+    files = {"image": f}
+    data = {"streamer_id": "streamer_005"}
     resp = requests.post(url, files=files, data=data)
 print(resp.status_code, resp.json())
+
 
 
 
